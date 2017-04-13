5203ee8cb2202f0afb8868d9f45c8168a868c770
Defjam <helpi9007@gmail.com>
2017-04-13 18:36:34 +0200

    update.sh hinzugefügt

---
+++ update.sh
@@ -0,0 +1,12 @@
+# @Author: march
+# @Date:   2017-04-13 18:27:36
+# @Last Modified by:   march
+# @Last Modified time: 2017-04-13 18:33:18
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+sudo apt-get dist-upgrade -y
+
+sudo apt-get autoremove -y
